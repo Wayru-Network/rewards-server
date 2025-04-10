777953e503764aa4438b6fc5b42206071dfa7957
david2899 <jironulload@gmail.com>
2025-04-10 10:57:41 -0600

    fixed interface

--- src/services/nfnodes/nfnode.service.ts
+++ src/services/nfnodes/nfnode.service.ts
@@ -76,7 +76,7 @@ const checkNFNodeEligibility = async (
 export const getEligibleWubiNFNodes = async (
     wayruDeviceId: string, 
     rewardSystemProgram: Program<RewardSystem> | null,
-): Promise<{ isEligible: boolean; nfnode: Pick<NfNode, 'id' | 'wayru_device_id' | 'model'> }> => {
+): Promise<{ isEligible: boolean; nfnode: Pick<NfNode, 'id' | 'wayru_device_id' | 'model' | 'solana_asset_id'> }> => {
     const nfnode = await getNfNodeByWayruDeviceId(wayruDeviceId);
     const { isEligible, reason } = await checkNFNodeEligibility(
         nfnode,
@@ -93,7 +93,7 @@ export const getEligibleWubiNFNodes = async (
 export const getEligibleWupiNFNodes = async (
     nfnodeId: number,
     rewardSystemProgram: Program<RewardSystem> | null
-): Promise<{ isEligible: boolean; nfnode: Pick<NfNode, 'id' | 'wayru_device_id' | 'model'> }> => {
+): Promise<{ isEligible: boolean; nfnode: Pick<NfNode, 'id' | 'wayru_device_id' | 'model' | 'solana_asset_id'> }> => {
     const nfnode = await getNFNodeById(nfnodeId);
     const { isEligible, reason } = await checkNFNodeEligibility(
         nfnode,
