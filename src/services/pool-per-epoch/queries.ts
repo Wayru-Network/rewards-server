e508bed5be2ff1e01feb3b362f870a6db52589e6
david2899 <jironulload@gmail.com>
2025-04-28 11:45:06 -0600

    start mainnet at 2025-04-29

--- src/services/pool-per-epoch/queries.ts
+++ src/services/pool-per-epoch/queries.ts
@@ -144,7 +144,7 @@ export const createCurrentPoolPerEpoch = async (epochParams?: Partial<PoolPerEpo
 
 export const getPoolPerEpochNumber = async (targetDate: Date) => {
     try {
-        const startMainnetDate = new Date('2025-04-27T00:00:00Z').valueOf()
+        const startMainnetDate = new Date('2025-04-29T00:00:00Z').valueOf()
         const start = new Date(startMainnetDate).valueOf()
         const target = new Date(targetDate).valueOf()
         const startCoolDownDate = new Date('2025-04-20T00:00:00Z').valueOf()
